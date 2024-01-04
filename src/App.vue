882b201e9e9661b92c387e6d1372fcc6dd6c33fd
Lapina Margarita <pmi.lapina.margarita@yandex.ru>
2024-01-04 16:41:20 +0300

    отрисованы кнопки и фон для первой страницы

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <CurrencyTable />
+  </div>
+</template>
+
+<script>
+import CurrencyTable from './components/CurrencyTable.vue';
+
+import './style.css';
+
+export default {
+  name: 'App',
+  components: { CurrencyTable },
+};
+</script>
---
+++ src/components/CurrencyTable.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="to-converter-button">КОНВЕРТЕР</button>
+  <button class="update-button"></button>
+</template>
+
+<script>
+export default {
+  name: 'CurrencyTable',
+};
+</script>
