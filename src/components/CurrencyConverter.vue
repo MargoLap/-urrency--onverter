c0f1334e9d00ff03dfe5529763e35ab12ac5cc63
Lapina Margarita <pmi.lapina.margarita@yandex.ru>
2024-01-08 13:36:43 +0300

    отрисован конвертер

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -3,7 +3,21 @@
   <button class="to-another-page" @click="$router.push({ name: 'Table' })">
     К ТАБЛИЦЕ
   </button>
-  <div class="converter-strings"></div>
+  <br class="clearer" />
+  <div class="converter-strings">
+    <h1 class="value-converter">90.5059</h1>
+    <h1 class="icon-converter">RUB</h1>
+    <div class="arrows">
+      <div class="arrow-1">
+        <div></div>
+      </div>
+      <div class="arrow-2">
+        <div></div>
+      </div>
+    </div>
+    <h1 class="value-converter">1</h1>
+    <h1 class="icon-converter">USD</h1>
+  </div>
 </template>
 
 <script>
