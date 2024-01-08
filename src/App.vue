70d547775bc4b8979af2a4cc3572112651d2552e
Lapina Margarita <pmi.lapina.margarita@yandex.ru>
2024-01-08 14:27:29 +0300

    добавлена анимация загрузки

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <RouterView />
+    <MyLoader v-if="loading" />
+    <RouterView v-else />
   </div>
 </template>
 
 <script>
 import './style.css';
+import MyLoader from './components/MyLoader.vue';
 
 export default {
   name: 'App',
+  data: () => ({
+    loading: true,
+    currency: null,
+  }),
+  components: { MyLoader },
 };
 </script>
